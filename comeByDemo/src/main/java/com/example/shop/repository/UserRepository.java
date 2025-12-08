db9d8406738c633d06c6170f0def7f5a87a8fc73
lecor <88902282+lecorrr@users.noreply.github.com>
2025-12-08 03:08:49 +0200

    Delete comeByDemo/src directory

--- comeByDemo/src/main/java/com/example/shop/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import com.example.shop.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> { Optional<User> findByEmail(String email); }
